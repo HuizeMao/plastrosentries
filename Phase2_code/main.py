d63810aa908a7135b2d184ba75ef1f4ed87c8604
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2020-01-04 13:03:52 +0100

    edit the code for phase two here

---
+++ Phase2_code/main.py
@@ -0,0 +1,14 @@
+#import necessary libraries
+from sense_hat import SenseHat
+from picamera import PiCamera
+from logzero import logger
+from math import sin,cos
+from time import sleep
+import datetime
+import logging
+import logzero
+import random
+import ephem
+import math
+import csv
+import os
