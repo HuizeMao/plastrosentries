79ed914d69fe918fc1f6dea6818150efa791c4bd
Huize Mao <Huizemao@gmail.com>
2020-06-30 11:15:56 +0200

    ai1.0

--- Phase3/AI_v1.0/Debug/Sample.py
+++ Phase3/AI_v1.0/Debug/Sample.py
@@ -1,8 +1,7 @@
 ###install keras-segmentation which contain all util required
 ###pip install keras-segmentation
+import tensorflow as tf
 import keras
-from keras_segmentation.models.model_utils import get_segmentation_model
-from keras_segmentation.models.unet import vgg_unet ##change 
 from keras.utils import plot_model
 from keras.layers import Input, Conv2D, MaxPooling2D, Dropout
 from keras.layers import concatenate,UpSampling2D
@@ -18,10 +17,23 @@ Y_train = #labels
 X_CV = #validation images(20%)
 Y_CV = #validation labels
 """
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+#theSession= tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+X_train = np.load("smallImg.npy")
+Y_train = np.load("smalllabel.npy")
+print(X_train.shape)
+print(Y_train.shape)
+X_train=X_train/255.0
+Y_train=Y_train/255.0
 
 def FCN(input_height,input_width):
     n_classes=6
-    img_input = Input(shape=(input_height,input_width , 3 ))
+    img_input = Input(shape=(input_height,input_width,3))
 
     ##Downsampling (Encoder)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(img_input)
@@ -51,32 +63,26 @@ def FCN(input_height,input_width):
     """
     Here conv1 is concatenated with conv4, and conv2 is concatenated with conv3. 
     """
-    out = Conv2D( n_classes, (1, 1) , padding='same')(conv5)
+    out = Conv2D( n_classes, (1, 1), activation='sigmoid', padding='same')(conv5)
     #model = get_segmentation_model(img_input ,  out ) # this would build the segmentation model
     model = Model(inputs = img_input, outputs = out, name='FCN')
-
+    
     return model
 
 
-model=FCN(972,1296)
-
-"""Preview Model"""
-plot_model(model, show_shapes=True,to_file='DraftModel.png')
-
-##Config
+model=FCN(296, 296)
+ ##Config    
 model.compile(loss='categorical_crossentropy',
-            optimizer = keras.optimizers.SGD(lr=0.01, decay=0, momentum=0, nesterov=False),
+            optimizer = keras.optimizers.Adam(lr=0.01),
             metrics=['acc',MeanIoU(num_classes=6)])
+##save best
+mc = ModelCheckpoint('DraftModel.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=
+                     
 ##Training
-history = model.fit(X_train, Y_train, batch_size = 1000,epochs = 1,verbose = 1, validation_data = (X_CV,Y_CV),shuffle=True)
-##safe
-model.save('DraftModel.h5')
+history = model.fit(X_train, Y_train, batch_size = 5,epochs = 15,verbose = 2, validation_split=0.2, shuffle=True,callbacks=[mc])
+##save
+#model.save('fakeFCN.h5')
 
-#evaluate
-preds = model.evaluate(X_CV, Y_CV)
-
-print ("Loss = " + str(preds[0]))
-print ("Test Accuracy = " + str(preds[1]))
 
 #summary/Learning curve
 model.summary()
@@ -89,8 +95,8 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
+plt.savefig("LCaccuracy.png") #LC=Learning Curve
+plt.clf()
 # summarize history for loss
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -98,4 +104,4 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+plt.savefig("LCloss.png")
